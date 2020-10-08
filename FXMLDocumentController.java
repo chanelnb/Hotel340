ffc6c4662ed7e5ce75527b620d290fa759106e95
amadhussain27 <71103781+amadhussain27@users.noreply.github.com>
2020-10-08 15:49:44 -0400

    Add files via upload

---
+++ FXMLDocumentController.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loginfx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author ahzaz
+ */
+public class FXMLDocumentController implements Initializable {
+    
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private Button btnSignup;
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    void Login(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Password(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Signup(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Username(ActionEvent event) {
+
+    }
+
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    
+}
