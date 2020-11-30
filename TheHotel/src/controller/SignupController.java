d7d82e50a5e0b772630d3a43fc5e67743ecd0913
Sogoltaheri <Sogoltaheri@users.noreply.github.com>
2020-11-30 11:20:36 -0500

    updated signup page and added 2 more models and home screen controller

--- TheHotel/src/controller/SignupController.java
+++ TheHotel/src/controller/SignupController.java
@@ -1,18 +1,40 @@
 package controller;
 
 import Main.Main;
+import Model.UserModel;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class SignupController implements Initializable{
 
+    //Creates a file for the stored results text file
+    
+    //creating models 
+    UserModel user = new UserModel();
+    
+    //getting result and pass to database
+    
+    
     @FXML
     private Label firstNameLabel;
 
@@ -64,6 +86,13 @@ public class SignupController implements Initializable{
     @FXML
     private Label passwordConfirmationLabel;
     
+    @FXML
+    private AnchorPane signupAnchorPane;
+
+    
+    @FXML
+    Button signupButton, loginButton;
+    
     private Main main;
     public void setMain(Main main) {
         this.main = main;
@@ -74,14 +103,47 @@ public class SignupController implements Initializable{
                 addressField.getText().isEmpty() || phoneNumberField.getText().isEmpty() ||
                 emailField.getText().isEmpty() || usernameField.getText().isEmpty() ||
                 passwordField.getText().isEmpty() || passwordConfirmationField.getText().isEmpty()){
-            
-        }
-        return false;
-    }
-    @FXML
-    void createUser(ActionEvent event) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setContentText("One or more fields are empty!");
+            alert.showAndWait();
 
+            return false;
+        }
+        else if(!passwordField.getText().equals(passwordConfirmationField.getText())){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setContentText("Password does not match!");
+            alert.showAndWait();   
+            
+            return false;
+        }
+        else
+            return true;
     }
+    
+//    public boolean userExist(){
+//        //waiting for connector
+//        
+//        return false;
+//    }
+
+    @FXML
+    void createUser(ActionEvent event) throws Exception{
+        if(checkFields())
+            //goes to login
+        try{
+            FXMLLoader loginLoader = new FXMLLoader(getClass().getResource("/Views/login.fxml"));
+            Parent loginParent = (Parent) loginLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(loginParent));
+            stage.show();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
